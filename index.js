fcf444e1e6b42fa14fd0a808c68492b1622ad484
kayke2409 <kaykeboldrin@gmail.com>
2025-04-07 18:04:25 -0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-black text-yellow-400">
-      <h1 className="text-5xl font-bold">Bem-vindo à Mercoders</h1>
-    </div>
-  );
-}
