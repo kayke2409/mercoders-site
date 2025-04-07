1f44e38f94269db2923258cc41f8a94473a347f1
kayke2409 <kaykeboldrin@gmail.com>
2025-04-07 18:04:57 -0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-black">
-      <h1 className="text-5xl font-bold text-yellow-400">Bem-vindo à Mercoders</h1>
-    </div>
-  )
-}
